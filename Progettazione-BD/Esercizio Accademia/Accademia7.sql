e506d76b2e55fb14875f078877fb87c98ceb80bc
PhilipVicari <philipvicari9@gmail.com>
2024-10-11 10:52:46 +0200

    agg2 Accademia

--- Progettazione-BD/Esercizio Accademia/Accademia7.sql
+++ Progettazione-BD/Esercizio Accademia/Accademia7.sql
@@ -25,20 +25,27 @@ select p.posizione, count(*)
 from Persona p
 where 
 
---DACOMPLETARE-------------
+----Da Completare----
 
 -- 4. Chi sono gli strutturati che hanno lavorato almeno 20 ore complessive in attività
 -- progettuali? Restituire tutti i loro dati e il numero di ore lavorate.
+
 select p.id, p.nome, p.cognome, p.posizione, p.stipendio, sum(ap.oreDurata) as Somma_ore
 from Persona p
 join AttivitaProgetto ap on p.id = ap.persona
 group by p.id, p.nome, p.cognome, p.posizione, p.stipendio
 HAVING sum(ap.oreDurata) >=20
+
 -- 5. Quali sono i progetti la cui durata è superiore alla media delle durate di tutti i
 -- progetti? Restituire nome dei progetti e loro durata in giorni.
+
 with Media_Durata(
   select avg()
 )
+
+----Da Completare----
+
+
 -- 6. Quali sono i progetti terminati in data odierna che hanno avuto attività di tipo
 -- “Dimostrazione”? Restituire nome di ogni progetto e il numero complessivo delle
 -- ore dedicate a tali attività nel progetto.
@@ -52,4 +59,4 @@ group by pr.id, pr.nome
 -- mero di assenze medio per malattia dei professori associati? Restituire id, nome e
 -- cognome del professore e il numero di giorni di assenza per malattia
 
-
+----Da Completare----
