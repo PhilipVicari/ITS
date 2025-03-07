9e3387d7d190e14e31debaf5db3159519b5bd7c1
PhilipVicari <philipvicari9@gmail.com>
2025-03-07 18:38:17 +0100

    agg

---
+++ GIT_Web/Web2/Processi Asincroni/lezione01.js
@@ -0,0 +1,24 @@
+function a(){
+    console.log("funzione a");
+}
+
+function b(){
+    console.log("funzione b");
+}
+
+function c(){
+    console.log("funzione c");
+}
+
+function d(){
+    console.log("funzione d");
+}
+
+function calcolo(){
+    a();
+    b();
+    c();
+    d();
+}
+
+calcolo();
